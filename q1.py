1b039176113284016878e6b3bee63707fa56851f
Magdalenessy <magdalenesiew@yahoo.com>
2025-12-14 19:07:51 +0800

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,27 @@
+
+def swap(x, y):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))) or isinstance(x, bool) or isinstance(y, bool):
+        return -1
+    # Swap using tuple unpacking (safer)
+    return y, x
+
+# Task 1a:
+result = swap("Apple", 10) -1:
+    print(-1)
+else:
+  x, y = result
+  print("Swapped values:")
+  print("x =", x)
+  print("y =", y)
+
+# Task 1b:
+result = swap(9, 17)
+if result == -1:
+    print(-1)
+else:
+  x, y = result
+if result ==
+  #print("Swapped values:")
+  print("x =", x)
+  print("y =", y)
+  
