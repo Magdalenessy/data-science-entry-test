67eb06ad6707b56f682e40195fb06957c39a131a
Magdalenessy <magdalenesiew@yahoo.com>
2025-12-27 00:30:26 +0800

    q1.py

--- q1.py
+++ q1.py
@@ -1,29 +1,22 @@
-def find_and_replace(lst, find_val, replace_val):
+def swap(x, y):
     """
     Task 1
-    - Create a function that searches for all occurrences of a value (find_val) in a given list (lst) and replaces them with another value (replace_val).
-    - lst must be a list.
-    - Return the modified list.
+    - Create a function that would swap the value of x and y using only x and y as variables.
+    - x and y must be numeric.
+    - Return -1 if x and y is not numeric, and
+    - print the swapped values if both x and y are numeric.
     """
-    
-    # initialise out as an empty list
-    # going through each value in the list lst
-    # if the value in the list is same as find_val then append the replace_val to the out list   
-    # if the value in the list is different from find_val then append the actual value to the out list
-    out = []
-    for i in lst:
-      if i == find_val:
-        out.append(replace_val)
-
-      else:
-        out.append(i)
-    return out
+    if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+      print("{0}, {1}".format(y,x))
+    else:
+      print("-1")
+    return
 
 
 # Task 2
-# Invoke the function "find_and_replace" using the following scenarios:
-# - [1, 2, 3, 4, 2, 2], 2, 5
-# - ["apple", "banana", "apple"], "apple", "orange"
+# Invoke the function "swap" using the following scenarios:
+# - "Apple", 10
+# - 9, 17
 
-print(find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
-print(find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
+swap("Apple", 10)
+swap(9, 17)
